6956fe8025d4b53e75330470c619044fc48c6f48
Kailan <Kailan1001101@gmail.com>
2025-02-14 22:44:43 +0200

    Account changes

--- src/SimpleBanking/Account.java
+++ src/SimpleBanking/Account.java
@@ -1,4 +1,37 @@
 package SimpleBanking;
 
 abstract class Account {
+
+    private String accountNumber;
+    private String accountHolder;
+    private double balance;
+
+
+    public Account(String accountNumber, String accountHolder, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = initialBalance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount);
+    public abstract void calculateInterest();
+
+    @Override
+    public String toString(){
+        return "Account Number: "+ accountNumber+", Holder: "+accountHolder+", Balance: "+balance;
+    }
+
 }
