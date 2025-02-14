c6a93c43bfb285db8ddbd7dab27d3640d1e23056
Kailan <Kailan1001101@gmail.com>
2025-02-14 22:42:52 +0200

    Creation of Account andChecking account class

---
+++ src/SimpleBanking/CheckingAccount.java
@@ -0,0 +1,24 @@
+package SimpleBanking;
+
+public class CheckingAccount extends Account {
+
+
+    public CheckingAccount(String accountNumber, String accountHolder, double initialBalance) {
+        super(accountNumber, accountHolder, initialBalance);
+    }
+
+    @Override
+    public void deposit(double amount) {
+
+    }
+
+    @Override
+    public void withdraw(double amount) {
+
+    }
+
+    @Override
+    public void calculateInterest() {
+
+    }
+}
